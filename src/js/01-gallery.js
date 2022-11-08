bc2cfb0f37fdd86a91378278aa1ef07a59faf2a0
Kolesanencore <kolesanencore@gmail.com>
2022-11-08 17:14:32 +0200

    Added completed homework #8

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,25 @@
+// // Описан в документации
+import SimpleLightbox from 'simplelightbox';
+// // Дополнительный импорт стилей
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+const galleryRef = document.querySelector('.gallery');
+
+const galleryMarkup = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<a class="gallery__item" href="${original}"><img class="gallery__image" src="${preview}" alt="${description}" /></a>`;
+  })
+  .join('');
+galleryRef.insertAdjacentHTML('beforeend', galleryMarkup);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
 console.log(galleryItems);
+
+console.log(lightbox);
